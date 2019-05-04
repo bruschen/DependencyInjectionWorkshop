e6d6835d64bcd06155d8ea9d1f0eda72c44fe9d4
bruschen <imchengr@gmail.com>
2019-05-04 10:49:20 +0800

    補驗證

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -63,6 +63,7 @@ namespace DependencyInjectionWorkshop.Models
             {
                 var httpClient2 = new HttpClient() { BaseAddress = new Uri("http://joey.dev/") };
                 var response2 = httpClient2.PostAsJsonAsync("api/FailCounter/Reset", accountId).Result;
+                response2.EnsureSuccessStatusCode();
                 if (response2.IsSuccessStatusCode == false)
                 {
                     throw new Exception($"web api FailCounter reset, accountId:{accountId}");
