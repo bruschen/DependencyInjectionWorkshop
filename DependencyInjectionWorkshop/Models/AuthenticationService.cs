515eebc46745bca1c9fd5eab64ac66824e586411
bruschen <imchengr@gmail.com>
2019-05-04 09:38:34 +0800

    初版

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,55 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string accountId, string actualPassword, string actualOneTimePassword)
+        {
+            string expectPassword = string.Empty;
+
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                expectPassword = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(actualPassword));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            expectPassword = hash.ToString();
+
+            //return hash.ToString();
+
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            var expectOneTImePassword = string.Empty;
+
+            if (response.IsSuccessStatusCode)
+            {
+                expectOneTImePassword = response.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
+
+            if (actualPassword != expectPassword || actualOneTimePassword != expectOneTImePassword)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DependencyInjectionWorkshop.Models;
 
 namespace MyConsole
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            string accountId = string.Empty;
+            string password = string.Empty;
+            string oneTimePassword = string.Empty;
+
+            var result = new AuthenticationService().Verify(accountId, password, oneTimePassword);
         }
     }
 }
